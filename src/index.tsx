5e7e335630113d2c39896e26f32f536492f7585c
root <root@DESKTOP-VJ31VUI.localdomain>
2024-02-23 17:32:01 +0500

    #fix-route

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { HashRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -10,9 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <HashRouter>
       <App />
-    </HashRouter>
   </React.StrictMode>
 );
 
